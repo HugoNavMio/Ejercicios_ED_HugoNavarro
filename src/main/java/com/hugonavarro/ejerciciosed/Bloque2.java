2d69a060e519b25bd714882097b489441224316c
Hugo_Navarro <tehspartanhugo@outlook.com>
2026-02-25 19:11:39 +0100

    Actualizado el Bloque2.java

--- src/main/java/com/hugonavarro/ejerciciosed/Bloque2.java
+++ src/main/java/com/hugonavarro/ejerciciosed/Bloque2.java
@@ -2,6 +2,7 @@ package com.hugonavarro.ejerciciosed;
 
 public class Bloque2 {
     public static int maximo(int[] datos) {
+        if (datos == null || datos.length == 0) throw new IllegalArgumentException();
         int max = 0;
         for (int i = 0; i < datos.length; i++) {
             if (datos[i] > max) {
